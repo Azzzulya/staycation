4998fb7ec86d69d335eb57be09628ae7beff4c1c
azzzulya <azisulyagifari27@gmail.com>
2021-04-08 11:09:48 +0700

    create header component and put to landingPage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-import Button from 'elements/Button';
+import Header from 'parts/Header';
 
 
 export default class LandingPage extends Component {
-  render(){
+  render() {
+    console.log(this.props)
     return (
-      <div>
-        
-      </div>
+     <>
+        <Header {...this.props}/>
+     </>
     )
   }
 }
