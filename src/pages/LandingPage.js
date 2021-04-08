35b71cb0acf7f36bd896e8704a113f3857f2c413
azzzulya <azisulyagifari27@gmail.com>
2021-04-08 16:04:19 +0700

    componenet hero with styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Header from 'parts/Header';
-
+import Hero from 'parts/Hero';
+import landingPage from "json/landingPage.json";
 
 export default class LandingPage extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class LandingPage extends Component {
     return (
      <>
         <Header {...this.props}/>
+        <Hero data={landingPage.hero} />
      </>
     )
   }
---
+++ src/utils/formatNumber.js
@@ -0,0 +1,5 @@
+
+  export default function numberFormat(number) {
+    const formatNumbering = new Intl.NumberFormat("id-ID");
+    return formatNumbering.format(number)
+  }
