ff8e27fc1a30c114ef823bceeff946b2a058aacd
azzzulya <azisulyagifari27@gmail.com>
2021-04-11 01:28:12 +0700

    section testimonial and styling

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,6 +3,7 @@ import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories";
+import Testimoni from "parts/Testimoni";
 
 
 import landingPage from "json/landingPage.json";
@@ -20,6 +21,8 @@ export default class LandingPage extends Component {
         <Hero refMostPicked={this.refMostPicked} data={landingPage.hero} />
         <MostPicked refMostPicked={this.refMostPicked} data={landingPage.mostPicked} />
         <Categories data={landingPage.categories} />
+        <Testimoni data={landingPage.testimonial}/>
+        
      </>
     )
   }
