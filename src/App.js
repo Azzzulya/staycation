bbc9c4b18da98897c3704d30ac358ae3f920d832
azzzulya <azisulyagifari27@gmail.com>
2021-04-15 20:09:38 +0700

    component input number

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react'
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 import "assets/scss/style.scss";
 import LandingPage from 'pages/LandingPage';
+import Example from "pages/Example";
 
 function App() {
   return (
     <div className="App">
      <Router>
-       <Route path="/" component={LandingPage}></Route>
+       <Switch>
+        <Route path="/" component={LandingPage}></Route>
+        <Route path="/example" component={Example} />
+       </Switch>
      </Router>
     </div>
   );
@@ -16,4 +20,3 @@ function App() {
 
 export default App;
 
-{/* <button className="btn btn-primary">tester</button> */}
