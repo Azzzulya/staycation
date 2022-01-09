e5df755933f130e856a2142dc01249d808687503
Azisulyagifari <53459509+Azzzulya@users.noreply.github.com>
2022-01-09 14:29:18 +0700

    integration checkout

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -16,7 +16,7 @@ import BookingInformation from "parts/Checkout/BookingInformation";
 import Payment from "parts/Checkout/Payment";
 import Completed from "parts/Checkout/Completed";
 
-import ItemDetails from "json/itemDetails.json";
+import {submitBooking} from 'store/actions/checkout'
 
 class Checkout extends Component {
   state = {
@@ -44,10 +44,34 @@ class Checkout extends Component {
     window.scroll(0, 0);
   }
 
+
+  _Submit = (nextStep) => {
+    const { data } = this.state
+    const { checkout } = this.props
+    const payload = new FormData()
+    payload.append("firstName", data.firstName);
+    payload.append("lastName", data.lastName);
+    payload.append("email", data.email);
+    payload.append("phoneNumber", data.phone);
+    payload.append("bankFrom", data.bankName);
+    payload.append("accountHolder", data.bankHolder);
+    payload.append("idItem", checkout._id);
+    payload.append("duration", checkout.duration);
+    payload.append("bookingStartDate", checkout.date.startDate);
+    payload.append("bookingEndDate", checkout.date.endDate);
+    // payload.append("bankId", checkout.bankId);
+    payload.append("image", data.proofPayment[0]);
+    this.props.submitBooking(payload).then( () => {
+      nextStep()
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+
   render() {
     const { data } = this.state;
     const { checkout, page } = this.props;
-    console.log(page)
+    console.log("page", data)
     if(!checkout) return (<div className="continer">
       <div className="row align-items-center justify-content-center text-center" style={{height: "100vh"}} >
         <div className="col-3">
@@ -153,7 +177,8 @@ class Checkout extends Component {
                           isBlock
                           isPrimary
                           hasShadow
-                          onClick={nextStep}
+                          
+                          onClick={() => this._Submit(nextStep)}
                         >
                           Continue to Book
                         </Button>
@@ -198,4 +223,4 @@ const mapStateToProps = (state) => ({
   page: state.page,
 });
 
-export default connect(mapStateToProps)(Checkout);
+export default connect(mapStateToProps, {submitBooking})(Checkout);
